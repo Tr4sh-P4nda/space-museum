8651717e775830c14b39114e3394d3b9f1832398
Steven Vergenz <vergenzs@gmail.com>
2016-09-26 15:43:56 -0400

    Add memorial wall

---
+++ viewer/memorial.js
@@ -0,0 +1,27 @@
+'use strict';
+
+window.memorial = {
+	
+	initialize: function(env, root, assets)
+	{
+		root.position.set(15.81, -14, 16);
+		/*root.add(new THREE.Mesh(
+			new THREE.CubeGeometry(1,1,1),
+			new THREE.MeshBasicMaterial()
+		));*/
+
+		var poster = new THREE.Mesh(
+			new THREE.PlaneGeometry(2.5,2.5),
+			new THREE.MeshBasicMaterial({map: assets.textures.wall})
+		);
+		poster.position.set(0,0,-2);
+		window.memorial = poster;
+		root.add(poster);
+	},
+
+	assets: {
+		textures: {
+			wall: 'textures/memorial.png'
+		}
+	}
+};
